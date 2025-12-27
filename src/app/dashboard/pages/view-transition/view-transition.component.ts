8b8b91b31070de93899d088f07039c35dc774d43
Ajutate <33647141+yoneljorge@users.noreply.github.com>
2025-12-27 15:27:51 -0500

    deploy

--- src/app/dashboard/pages/view-transition/view-transition.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from "@shared/title/title.component";
 
 @Component({
-  imports: [],
-  templateUrl: './view-transition.component.html',
-  styles: ``,
+  imports: [TitleComponent],
+  template: `
+    <app-title title="View Transition"/>
+
+    <section>
+
+    <img 
+    srcset="https://picsum.photos/id/237/200/300"
+    alt="Picsum"
+    width="200"
+    height="300"
+    >
+
+    <div 
+    class="bg-blue-500 w-56 h-56">
+      
+    </div>
+    </section>
+  `
 })
 export default class ViewTransitionComponent {
 
