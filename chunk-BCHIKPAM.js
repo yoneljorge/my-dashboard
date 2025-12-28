ba1332b821de55e6a6ec8b076eed3de0fe9b177c
Ajutate <33647141+yoneljorge@users.noreply.github.com>
2025-12-27 23:02:49 -0500

    Auto-generated commit

--- chunk-BCHIKPAM.js
+++
@@ -1,1 +0,0 @@
-import{$a as s,ab as a,ib as r,ya as o}from"./chunk-T3H5EJAW.js";var t=class n{static \u0275fac=function(e){return new(e||n)};static \u0275cmp=o({type:n,selectors:[["ng-component"]],decls:2,vars:0,template:function(e,p){e&1&&(s(0,"p"),r(1,"users works!"),a())},encapsulation:2})};export{t as default};
